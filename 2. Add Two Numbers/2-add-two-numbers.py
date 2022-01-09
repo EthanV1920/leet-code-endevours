035779affb6885a19fde1aa675d6b32797f22db3
Ethan Vosburg <ethan@vosburg.us>
2022-01-09 12:36:53 -0800

    Linked List Output Working, resolved starting index problem improperly. Not accepted for all test cases.

--- 2. Add Two Numbers/2-add-two-numbers.py
+++ 2. Add Two Numbers/2-add-two-numbers.py
@@ -1,19 +1,47 @@
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = ListNode()
+
+    def  begin(self):
+        return(self.head.next)
+
+    def append(self,value):
+        new_val = ListNode(value)
+        current = self.head
+
+        while current.next != None:
+            current = current.next
+
+        current.next = new_val
+
+    def display(self):
+        displayArray = []
+        current = self.head
+        
+        while current != None:
+            displayArray.append(current.val)
+            current = current.next
+        
+        print(displayArray)
 
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        outputArray = []
+        ListOut = LinkedList()
+        ListOut.display()
         # returnOutput = ListNode()
         # intStore = ListNode() 
         carryInt = 0
 
-        while l1 != None:
+        while l1 and l2 != None:
 
             if (l1.val + l2.val) >= 10:
                 intStore = (l1.val + l2.val) % 10
@@ -21,7 +49,13 @@ class Solution:
 
                 carryInt = 1
 
-                outputArray.append(intStore)
+                ListOut.append(intStore)
+            
+            elif l1 == None:
+                ListOut.append(l2.val)
+                
+            elif l2 == None:
+                ListOut.append(l1.val)
             
             else:
                 intStore = (l1.val + l2.val) + carryInt
@@ -29,20 +63,15 @@ class Solution:
 
                 carryInt = 0
 
-                outputArray.append(intStore)
+                ListOut.append(intStore)
 
             print("List 1: " + str(l1.val))
             print("List 2: " + str(l2.val))
             print("Carry: " + str(carryInt))
-            print(outputArray)
+            ListOut.display()
             # outptArray = outputArray.next
             l1 = l1.next
             l2 = l2.next
 
-        node3 = ListNode(outputArray[2],None)
-        node2 = ListNode(outputArray[1],node3)
-        node1 = ListNode(outputArray[0],node2)
         
-        # linkedListOut = ListNode(outputArray[0],ListNode(outputArray[1],ListNode(outputArray[2])))
-
-        return(node1)
+        return(ListOut.begin())
