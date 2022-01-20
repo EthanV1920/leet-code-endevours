5d9bc5cdc9df85ba9f053d4d470a78dc67d5e22c
Ethan Vosburg <ethan@vosburg.us>
2022-01-20 12:23:11 -0800

    Incorrect Answer #3
    Going about the answer in the wrong way. Need to interpret a smarter way to find redundant sub-strings.

--- 3. Longest Substring Without Repeating Characters/3-longest-substring-without-repeating-characters.py
+++ 3. Longest Substring Without Repeating Characters/3-longest-substring-without-repeating-characters.py
@@ -1,3 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
+        stringCount = 0
+        largestString = 0
+        prevChar = None
+
+        for i in range(len(s)):
+            curChar = s[i]
+
+            if curChar != prevChar:
+                stringCount +=1
+                print(f"INFO: Checked {prevChar} and {curChar} stringCount: {stringCount}")
+                prevChar = curChar
+            else:
+                largestString = stringCount
+                stringCount = 0
+                prevChar = curChar
+                print("INFO: Not equal, restarting count" + str(stringCount))
+                return
+
+     
+        return(largestString)
